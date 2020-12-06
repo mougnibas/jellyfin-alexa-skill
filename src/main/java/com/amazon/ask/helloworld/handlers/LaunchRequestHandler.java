d377d4b32e21f3f318f43c8cd59c7710c6426e42
Yoann MOUGNIBAS <yoann@mougnibas.fr>
2020-12-06 18:37:14 +0100

    Adding first request handler

---
+++ src/main/java/com/amazon/ask/helloworld/handlers/LaunchRequestHandler.java
@@ -0,0 +1,33 @@
+package com.amazon.ask.helloworld.handlers;
+
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.LaunchRequest;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.request.Predicates;
+
+import java.util.Optional;
+
+/**
+ * This request handler can only handle the request if a user made a request to an Alexa skill, but did not provide a specific intent.
+ */
+public class LaunchRequestHandler implements RequestHandler {
+
+    @Override
+    public boolean canHandle(HandlerInput input) {
+        return input.matches(Predicates.requestType(LaunchRequest.class));
+    }
+
+    @Override
+    public Optional<Response> handle(HandlerInput input) {
+
+        String speechText = "Welcome to the Alexa Skills Kit, you can say hello";
+
+        return input.getResponseBuilder()
+                .withSpeech(speechText)
+                .withSimpleCard("HelloWorld", speechText)
+                .withReprompt(speechText)
+                .build();
+    }
+
+}
